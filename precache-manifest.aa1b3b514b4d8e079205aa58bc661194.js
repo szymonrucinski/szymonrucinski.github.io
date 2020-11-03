19e193543ad620b24aed0b626356eaf0126a9851
Szymon Ruciński <44577190+szymonrucinski@users.noreply.github.com>
2020-11-03 11:11:37 +0100

    Add files via upload

---
+++ precache-manifest.aa1b3b514b4d8e079205aa58bc661194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8926aaba8b02a191e0f2",
+    "url": "./static/css/main.b281a1e6.chunk.css"
+  },
+  {
+    "revision": "8926aaba8b02a191e0f2",
+    "url": "./static/js/main.3ebde2a4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "./static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "ddd1fb4f995e26c77d1e687b1e06edf3",
+    "url": "./index.html"
+  }
+];
